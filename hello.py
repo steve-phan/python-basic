c2e9433249ad21d3f34767f396a89f69d0bd13d4
vUnAmp <phanhaingoc@gmail.com>
2021-07-28 10:47:52 +0200

    let learn some python

---
+++ hello.py
@@ -0,0 +1,10 @@
+print("What is your Name?")
+name = input()
+print("Hello " + name + " How are you?")
+answer = input()
+if answer == "good":
+    print("Oh my god " + name + " You are very strong")
+if answer == "bad":
+    print("Mannnne " + name + "What a pity")
+else:
+    print("I dont understand,, what are u saying.. sorry and byeeeeeee")
